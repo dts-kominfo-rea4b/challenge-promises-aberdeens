45074e2733395e325f7c06ae69a7cdb1c42dfd32
aberdeens <54465985+aberdeens@users.noreply.github.com>
2022-09-18 23:40:35 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,13 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async() => {
-  try{
-    const promiseTheaterIXX = await promiseTheaterIXX();
-    const promiseTheaterVGC = await promiseTheaterVGC();
-  } catch (err){
-    console.log(err);
-  }
+const promiseOutput = (value) => {
+  return new Promise((resolve) => {
+    const theaterResult = [];
+
+    promiseTheaterIXX().then((data) => {
+      theaterResult.push(...data);
+
+      promiseTheaterVGC().then((data) => {
+        theaterResult.push(...data);
+
+        if (value === 'marah') {
+          const marah = theaterResult.filter((item) => item.hasil === 'marah');
+
+          resolve(marah.length);
+        } else if (value === 'tidak marah') {
+          const tidakMarah = theaterResult.filter(
+            (item) => item.hasil === 'tidak marah'
+          );
+
+          resolve(tidakMarah.length);
+        }
+      });
+    });
+  });
 };
 
 module.exports = {
