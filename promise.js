3475514a449cdae7afe3c154535cccad921fc8d8
aberdeens <54465985+aberdeens@users.noreply.github.com>
2022-09-18 23:07:31 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = new Promise((resolve,reject) => {
+  if("marah"){
+    promiseOutput = 4;
+  }
+  else if ("tidak marah"){
+    promiseOutput = 2;
+  }
+});
 
 module.exports = {
   promiseOutput,
